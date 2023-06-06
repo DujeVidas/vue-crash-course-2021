90fa488f78139632636e809bbff8d196af238a87
Duje Vidas <dujevidas123@gmail.com>
2023-06-06 12:29:36 +0200

    zavrsen app iz crash coursa

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>Version 1.0.0</h3>
+        <router-link to="/">Go back</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
